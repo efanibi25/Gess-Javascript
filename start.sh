d37a840c8881fdc1750cd4e3574a316afe6c23fe
efanibi25 <58971180+efanibi25@users.noreply.github.com>
2023-11-11 21:08:26 -0600

    make redis save more agressive

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-concurrently "docker run --name redis --rm -p 6379:6379 -v ./data:/data redis:7.2.2" "nodemon ./server/socket/listeners.js" "nodemon ./server/phaser.js" 
+concurrently "docker run --name redis --rm -p 6379:6379 -v ./data:/data redis:7.2.2 --save 15 1" "nodemon ./server/socket/listeners.js" "nodemon ./server/phaser.js" 
 #concurrently "nodemon ./server/socket/listeners.js" "./server/nodemon phaser.js"
