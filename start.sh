ba781a976d2be4de19a311d12e3185c9a7592c38
johnsmih <you@example.com>
2025-04-10 18:21:17 -0500

    fix redis local host

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-concurrently "docker run --name redis --rm -p 6379:6379 -v ./data:/data redis:7.2.2 --save 15 1" "nodemon ./server/socket/listeners.js" "nodemon ./server/phaser.js" 
-#concurrently "nodemon ./server/socket/listeners.js" "./server/nodemon phaser.js"
+concurrently "docker run --name redis --rm -p 127.0.0.1:6379:6379 -v ./data:/data redis:7.2.2 --save 15 1" "nodemon ./src/server/socket/listeners.js" "nodemon ./src/server/phaser.js" 
+#no redis example
+#concurrently "nodemon ./src/server/socket/listeners.js" "./src/server/nodemon phaser.js"
+
